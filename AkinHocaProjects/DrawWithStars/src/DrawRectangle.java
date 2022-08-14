82cb6cbc9b104cbac15625b1e6f96743023f3850
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2022-08-14 11:25:35 +0300

    An another for loop example

--- AkinHocaProjects/DrawWithStars/src/DrawRectangle.java
+++ AkinHocaProjects/DrawWithStars/src/DrawRectangle.java
@@ -1,32 +1,38 @@
-import java.util.Scanner;
-
 public class DrawRectangle {
 
     public static void main(String[] args) {
         DrawRectangle r = new DrawRectangle();
-        r.drawVoidRectangle();
+        r.drawVoidRectangle(10,8    );
     }
 
-    public void drawVoidRectangle() {
-        Scanner scanner = new Scanner(System.in);
+    public void drawVoidRectangle(int wid, int len) {
+        /*Scanner scanner = new Scanner(System.in);
         System.out.print("Length: ");
         int len = scanner.nextInt();
         System.out.print("Width: ");
-        int wid = scanner.nextInt();
+        int wid = scanner.nextInt();*/
 
-        for (int i = 1; i < len; i++) {
-            System.out.print("* ");
-            /*if (i == uu-1) {
-                System.out.println("*");
-            }*/
-        }
-        for (int j = 0; j < wid; j++) {
-            System.out.println("*");
-            if (j == (wid - 2)) {
-                for (int i = 1; i < len; i++) {
-                    System.out.print("* ");
+        for (int i = 0; i < wid; i++) {
+            for (int j = 0; j < len; j++) {
+                if (i == 0 | i == wid - 1) {
+                    System.out.print("*");
+                    if (j == len - 1) {
+                        System.out.println();
+                    }
+                }
+                else {
+                    if (j == 0) {
+                        System.out.print("*");
+                    }
+                    else if (j == len - 1) {
+                        System.out.println("*");
+                    }
+                    else {
+                        System.out.print(" ");
+                    }
                 }
             }
         }
+
     }
 }
